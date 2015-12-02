fd8a6f3cc03d933ea6b459856138cb6111c393ff
rafaeling <rafaelingugr@gmail.com>
2015-12-02 16:45:44 +0100

    Plane

--- altas/src/altas/Plane.java
+++ altas/src/altas/Plane.java
@@ -20,14 +20,13 @@ import java.util.logging.Logger;
  */
 public class Plane extends Thread{
     
-    // stream de escritura (por aquí se envía los datos al cliente)
-    PrintWriter outPrinter;
-                
-    // stream de lectura (por aquí se recibe lo que envía el cliente)
-    BufferedReader inReader;
-                
+    
     // Para guardar la lectura
-    String buferRecepcion="";  
+    String respuesta="";  
+    
+    String land = "land";
+    
+    boolean land_plane = false;
 		
     // Nombre del host donde se ejecuta el servidor:
     String host="localhost";
@@ -38,43 +37,88 @@ public class Plane extends Thread{
     // Socket para la conexión TCP
     Socket socketServicio=null;
     
+    int i = 0;
     
     public void run() {
         try {
             // Creamos un socket que se conecte a "host" y "port":
-           
-            socketServicio =new Socket (host,port);
 
-            inReader = new BufferedReader(new InputStreamReader(socketServicio.getInputStream()));           
-                        
-            outPrinter = new PrintWriter(socketServicio.getOutputStream(),true);
-			
+            while(!land_plane){
+                
+                socketServicio =new Socket (host,port);
+                
+                i++;
+                
+                //socketServicio =new Socket (host,port);
+                
+                // stream de escritura (por aquí se envía los datos al cliente)
+                PrintWriter outPrinter = new PrintWriter(socketServicio.getOutputStream(),true);
+                
+    // stream de lectura (por aquí se recibe lo que envía el cliente)
+    //BufferedReader inReader = new BufferedReader(new InputStreamReader(socketServicio.getInputStream()));  
+                
 
-            // Enviamos el array outPrinter;
+                // Enviamos peticion de aterrizar
 
-            outPrinter.println("Al monte del volcán debes ir sin demora\0");
+                outPrinter.println("land");
 
-            // Aunque le indiquemos a TCP que queremos enviar varios arrays de bytes, sólo
-            // los enviará efectivamente cuando considere que tiene suficientes datos que enviar...
-            // Podemos usar "flush()" para obligar a TCP a que no espere para hacer el envío:
+                //System.out.println("Numero: " + i);
+                
+                // Aunque le indiquemos a TCP que queremos enviar varios arrays de bytes, sólo
+                // los enviará efectivamente cuando considere que tiene suficientes datos que enviar...
+                // Podemos usar "flush()" para obligar a TCP a que no espere para hacer el envío:
 
-              outPrinter.flush();
+                outPrinter.flush();
 
-            // Leemos la respuesta del servidor. Para ello le pasamos un array de bytes, que intentará
-            // rellenar. El método "read(...)" devolverá el número de bytes leídos.
+                // Leemos la respuesta del servidor. Para ello le pasamos un array de bytes, que intentará
+                // rellenar. El método "read(...)" devolverá el número de bytes leídos.
 
-            buferRecepcion = inReader.readLine();
+                //buferRecepcion = inReader.readLine();
 
-            // Hay que reservar memoria para almacenar lo leído
-            // Intenta leer tantos bytes como posiciones tiene el
-            // array de bytes, aunque es posible que no haya
-            // tantos datos, y sólo se lean bytesLeidos:
+                // Hay que reservar memoria para almacenar lo leído
+                // Intenta leer tantos bytes como posiciones tiene el
+                // array de bytes, aunque es posible que no haya
+                // tantos datos, y sólo se lean bytesLeidos:
 
-            System.out.println("Recibido: ");
-            System.out.print(buferRecepcion);
+                //System.out.println("Recibido: ");
+                //System.out.print(buferRecepcion);
+                
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(socketServicio.getInputStream())); 
+                
+                respuesta = inReader.readLine();
+                
+                if (respuesta.equals(land)){
+                    
+                    System.out.println("Puede Aterrizar");
+                    
+                    PrintWriter land_accepted = new PrintWriter(socketServicio.getOutputStream(),true);
 
-            socketServicio.close();
+                    outPrinter.println("land_accepted");
+                    
+                    outPrinter.flush();
+                    
+                    land_plane = true;
+                    
+                }
+                
+                if(land_plane)
+                {
+                    BufferedReader inputStream = new BufferedReader(new InputStreamReader(socketServicio.getInputStream()));
+                 
+                    String accept = inputStream.readLine();
+                    
+                    // aqui decido aleatoriamente si puedo o no
+                    
+                    PrintWriter land_accepted = new PrintWriter(socketServicio.getOutputStream(),true);
 
+                    outPrinter.println(accept);
+                    
+                    outPrinter.flush();
+                }
+
+            }
+               socketServicio.close(); 
+            
                 // Excepciones:
         } catch (UnknownHostException e) {
                 System.err.println("Error: Nombre de host no encontrado.");
